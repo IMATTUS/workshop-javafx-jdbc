257e56482245c3a4e0fdaf966db7540725cd298f
Ibrahim Mattus Neto <2496339+IMATTUS@users.noreply.github.com>
2022-04-29 07:13:17 -0300

    Creating About View

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,13 +1,22 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
-public class MainViewController implements Initializable{
+public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemSeller;
@@ -15,26 +24,52 @@ public class MainViewController implements Initializable{
 	private MenuItem menuItemDepartment;
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
 		System.out.println("onMenuItemDepartmentAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
-	
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
+	// synchronized para garantir que o processamento não será interrompido durante o 
+	// multi-thread
+	private synchronized void loadView(String absoluteName) {
+		try {
+			/*
+			 * Vai carregar o fxml 'about.fxml'
+			 * depois de carregar ele, vai colocar na variavel newVbox
+			 * vai pegar a referencia pra cena principal e pega o conteudo do VBox de lá
+			 * guarda o menu principal (do VBox da cena principal) em uma variável separada
+			 * apaga todo o conteudo do VBox principal
+			 * adiciona o menu novamente, e o conteudo do VBox do About.xml - variavel newVBox
+			 */
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVbox = loader.load();
+			Scene mainScene = Main.getMainScene();
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node mainMenu = mainVbox.getChildren().get(0);
+			mainVbox.getChildren().clear();
+			mainVbox.getChildren().add(mainMenu);
+			mainVbox.getChildren().addAll(newVbox.getChildren());
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading View", e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
